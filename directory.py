9330caba05ea5832347a95ca66d0ee94ca063302
Gabrilliant <gevorkgabrielan@gmail.com>
2022-04-17 14:21:26 +0300

    + argv & lower

--- directory.py
+++ directory.py
@@ -1,6 +1,7 @@
 from search import search
+from sys import argv
 
-catalog = input()
+catalog = argv[1]
 
 for basepath in ('C:\Program Files', 'C:\Program Files (x86)'): 
     search(basepath, catalog)
--- search.py
+++ search.py
@@ -3,5 +3,5 @@ import fnmatch
 
 def search(base, cat):
     for dirpath, dirnames, files in os.walk(base):
-        if cat in dirpath and not fnmatch.fnmatch(dirpath, f'*{cat}*\\*'):
+        if cat.lower() in dirpath.lower() and not fnmatch.fnmatch(dirpath, f'*{cat}*\\*'):
             print(dirpath)
