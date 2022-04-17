d1b10fbe8a8a4d52bdc63448a8f714dc722e5475
Gabrilliant <gevorkgabrielan@gmail.com>
2022-04-17 22:32:30 +0300

    last_commit

--- main.py
+++ main.py
@@ -5,11 +5,6 @@ import win32con
 
 name = argv[-1]
 
-# pf_cat = search('C:\Program Files', name)
-# pfx86_cat = search('C:\Program Files (x86)', name)
-# pf = parse_reg(name, win32con.HKEY_LOCAL_MACHINE, win32con.KEY_WOW64_32KEY)
-# pfx86 = parse_reg(name, win32con.HKEY_LOCAL_MACHINE, win32con.KEY_WOW64_64KEY)
-
 catalogs_cat = ['C:\Program Files', 'C:\Program Files (x86)']
 catalogs_ree = [win32con.KEY_WOW64_32KEY, win32con.KEY_WOW64_64KEY]
 ans = []
